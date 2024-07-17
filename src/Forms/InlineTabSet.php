1a227c9c795314440fcbd045656ed7e26d0640e7
Luke Fromhold <luke@fromhold.io>
2024-07-18 08:21:51 +1000

    Require field css

--- src/Forms/InlineTabSet.php
+++ src/Forms/InlineTabSet.php
@@ -3,8 +3,13 @@
 namespace Fromholdio\InlineTabSet\Forms;
 
 use SilverStripe\Forms\TabSet;
+use SilverStripe\View\Requirements;
 
 class InlineTabSet extends TabSet
 {
-
+    public function FieldHolder($properties = [])
+    {
+        Requirements::css('fromholdio/silverstripe-inlinetabset:client/inlinetabset.css');
+        return parent::FieldHolder($properties);
+    }
 }
